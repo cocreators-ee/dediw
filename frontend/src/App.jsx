e427bf3d0d21cb58d0143f4ca5bcd31b8c47f7e0
Janne Enberg <janne.enberg@lietu.net>
2023-09-13 21:24:06 +0300

    Seems to kinda run

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import {Tldraw, useFileSystem} from '@tldraw/tldraw'
+
+function App() {
+    const fileSystemEvents = useFileSystem()
+
+    return (
+        <div className="tldraw">
+            <Tldraw {...fileSystemEvents} />
+        </div>
+    )
+}
+
+export default App
